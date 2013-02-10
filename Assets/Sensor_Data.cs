556bb61071af578147579a7890e3a5184b6c4f4f
Tran <tran.thirteen@gmail.com>
2013-02-09 23:45:37 -0500

    added a C# file which will be used for coding sensors, currently displays Hello World in Unity Console

---
+++ Assets/Sensor_Data.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections;
+
+public class Sensor_Data : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //System.Console.WriteLine("Hello World!"); this won't print to console in unity
+        //however this line will
+        print("Hello World!!");
+        //sensor data will print in this method
+	}
+}
