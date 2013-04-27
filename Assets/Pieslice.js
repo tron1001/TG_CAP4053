8f29ddff7e2698386abd023fc0c14faa48f3bb89
Tran <tran.thirteen@gmail.com>
2013-04-27 19:22:07 -0400

    almost doneish

--- Assets/Pieslice.js
+++ Assets/Pieslice.js
@@ -6,10 +6,7 @@ each npc show up in which slice if the npc is within range
 */
 
 public var cObject : Transform;
-public var front : int;
-public var back : int;
-public var right : int;
-public var left : int;
+public var count : int;
 
 function Update () {
 	
@@ -18,30 +15,27 @@ function Update () {
 	var bl = transform.TransformDirection(Vector3(1,0,1));
 	var br = transform.TransformDirection(Vector3(1,0,-1));
 	//draws the 4 lines seperating the pie slices
-	Debug.DrawRay(transform.position, fl * 3, Color.green);
-	Debug.DrawRay(transform.position, fr * 3, Color.blue);
-	Debug.DrawRay(transform.position, bl * 3, Color.red);
-	Debug.DrawRay(transform.position, br * 3, Color.yellow);
+	//Debug.DrawRay(transform.position, fl * 40, Color.green);
+	//Debug.DrawRay(transform.position, fr * 40, Color.blue);
+	//Debug.DrawRay(transform.position, bl * 6, Color.red);
+	//Debug.DrawRay(transform.position, br * 6, Color.yellow);
 	//these variables will be used to store the # of agents in each
 	//pie slice section
-	front = 0;
-	back = 0;
-	left = 0;
-	right = 0;
+	count = 0;
+	
 	//finds all agents with the tag, in other words, find all the npc
 	var npc : GameObject[];
-	npc = GameObject.FindGameObjectsWithTag("Agent");
+	npc = GameObject.FindGameObjectsWithTag("Player");
 	//goes through each of the npc found and store in the GameObject array
 	for (var nc : GameObject in npc) {
 		findnpc (nc);
 	}
-	print("Front: " + front);
-	print("Left: " + left);
-	print("Back: " + back);
-	print("Right: " + right);
+	print("CUrrent Position: " + transform.position);
+	print("Players: " + count);
 }
 
 function findnpc(nc) {
+	print("In function");
 	//
 	var cPos : Vector3;
 	var npcPos : Vector3;
@@ -54,7 +48,9 @@ function findnpc(nc) {
 	npcPos = nc.transform.position;
 	//
 	dist = Vector3.Distance(cPos, npcPos);
-	if(dist < 3){
+	
+	
+	if(dist < 40){
 		//finds the distance between the npc and the player character
 		xdist = npcPos.x - cPos.x;
 		zdist = npcPos.z - cPos.z;
@@ -65,14 +61,9 @@ function findnpc(nc) {
 			ang = (ang * -1) % 360;
 		else
 			ang = ang % 360;
-		//will keep count of how many agents are in which part of the pie slice
-		if(ang <= 45 || ang >= 315)
-			front++;
-		else if(ang > 45 && ang <= 135)
-			left++;
-		else if(ang > 135 && ang <= 225)
-			back++;
-		else if(ang > 225 && ang < 315)
-			right++;
+		if(dist < 6 && (ang > 45 && ang < 315))
+			count++;
+		else if(ang <= 45 || ang >= 315)
+			count++;
 	}
 }
--- Assets/Raycasting.js
+++ Assets/Raycasting.js
@@ -5,29 +5,29 @@ character and the wall if the feeler comes into contact with the wall
 */
 function Update () {
 	//sets the 3 vectors for the feelers
-   var right = transform.TransformDirection(Vector3(-1,-1,0));
-   var front = transform.TransformDirection(Vector3.left);
-   var left = transform.TransformDirection(Vector3(-1,1,0));
+   var right = transform.TransformDirection(Vector3(1,0,1));
+   var front = transform.TransformDirection(Vector3(0,0,1));
+   var left = transform.TransformDirection(Vector3(-1,0,1));
    
    var hit : RaycastHit;  
    var box : Transform;
    var dist : float;
     //draws the 3 feelers in front of the player character 
-   Debug.DrawRay(transform.position, right * 2, Color.green);
-   Debug.DrawRay(transform.position, front * 3, Color.yellow);
-   Debug.DrawRay(transform.position, left * 2, Color.red);
+   //Debug.DrawRay(transform.position, right * 2, Color.green);
+   //Debug.DrawRay(transform.position, front * 3, Color.yellow);
+   //Debug.DrawRay(transform.position, left * 2, Color.red);
  	//if the feelers hit the wall, the if statement will trigger
  	//returning the distance from the player character to the wall
  	//and which feeler triggered it
-   if(Physics.Raycast(transform.position, right, hit, 2)){
+   if(Physics.Raycast(transform.position, right, hit, 30)){
       	dist = hit.distance;
       	Debug.Log("Green Hit " + dist);
    }
-   if(Physics.Raycast(transform.position, front, hit, 3)){
+   if(Physics.Raycast(transform.position, front, hit, 40)){
    		dist = hit.distance;
    		Debug.Log("Yellow Hit " + dist);
    }
-   if(Physics.Raycast(transform.position, left, hit, 2)){
+   if(Physics.Raycast(transform.position, left, hit, 30)){
    		dist = hit.distance;
    		Debug.Log("Red Hit " + dist);
    }
